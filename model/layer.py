feb784b0450b7a4ffc3885333ab8d18395e8fee1
Shuai <s.wang3@uva.nl>
2024-02-29 11:39:00 +0100

    deleting directory

--- model/layer.py
+++ model/layer.py
@@ -10,53 +10,6 @@ import pdb
 
 
 
-class EarlyStopping:
-    """Early stops the training if validation loss doesn't improve after a given patience."""
-
-    def __init__(self, patience, verbose=False, delta=0, save_path='checkpoint.pt'):
-        """
-        Args:
-            patience (int): How long to wait after last time validation loss improved.
-                            Default: 7
-            verbose (bool): If True, prints a message for each validation loss improvement.
-                            Default: False
-            delta (float): Minimum change in the monitored quantity to qualify as an improvement.
-                            Default: 0
-        """
-        self.patience = patience
-        self.verbose = verbose
-        self.counter = 0
-        self.best_score = None
-        self.early_stop = False
-        self.val_loss_min = np.Inf
-        self.delta = delta
-        self.save_path = save_path
-
-    def __call__(self, val_loss, model):
-
-        score = -val_loss
-
-        if self.best_score is None:
-            self.best_score = score
-            self.save_checkpoint(val_loss, model)
-        elif score < self.best_score - self.delta:
-            self.counter += 1
-            print(
-                f'EarlyStopping counter: {self.counter} out of {self.patience}')
-            if self.counter >= self.patience:
-                self.early_stop = True
-        else:
-            self.best_score = score
-            self.save_checkpoint(val_loss, model)
-            self.counter = 0
-
-    def save_checkpoint(self, val_loss, model):
-        """Saves model when validation loss decrease."""
-        if self.verbose:
-            print(
-                f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
-        torch.save(model, self.save_path)
-        self.val_loss_min = val_loss
 
 
 class HyperGraphAttentionLayerSparse(nn.Module):
@@ -207,31 +160,3 @@ class HyperGraphAttentionLayerSparse(nn.Module):
         return self.__class__.__name__ + ' (' + str(self.in_features) + ' -> ' + str(self.out_features) + ')'
 
 
-def build_hyperedge(E_list):
-    """
-    input hyperedge list
-    return incidence matrix (node_num * hyperedge_num)!! load this into the transformer.
-    get the transformer from the .layer code and try to make it for heterogenous
-    """
-    # k is index and V are nodes
-    # for k, v in E.items():
-    #     E[k] = list(v)
-    #     if k not in E[k]:
-    #         E[k].append(k)
-    # E = dict(sorted(E.items()))
-
-    # Get E features of nodes
-    # a = list(E.values())
-    map(max, E_list)
-    list(map(max, E_list))
-    max_val = max(map(max, E_list))
-    edge_feature = np.zeros((len(E_list), max_val + 1))
-
-    # Build E matrix
-    for row, list_ in enumerate(E_list):
-        for i in np.arange(len(list_)):
-            edge_feature[row, E_list[row][i]] = 1  # (4057, 26128)
-
-    edge_feature = np.transpose(edge_feature)
-    edge_feature = torch.from_numpy(edge_feature)
-    return edge_feature
